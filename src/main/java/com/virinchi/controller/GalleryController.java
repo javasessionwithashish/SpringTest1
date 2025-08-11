ca7ef534f6aa46e5f2696624216ed3e4f72904e4
ashish <ashish.koirala004@gmail.com>
2025-08-11 07:52:13 +0545

    august 11

--- src/main/java/com/virinchi/controller/GalleryController.java
+++ src/main/java/com/virinchi/controller/GalleryController.java
@@ -4,9 +4,11 @@ import com.virinchi.model.ImageClass;
 import com.virinchi.repository.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -19,13 +21,15 @@ public class GalleryController {
     private ImageRepository imageRepo;
 
     @GetMapping("/gallery")
-    public String gallery() {
+    public String gallery(Model m) {
+
+m.addAttribute("imageList",imageRepo.findAll());
         return "gallery";
     }
 
 
     @PostMapping("/gallery")
-    public String galleryPost(@RequestParam("image") MultipartFile image)
+    public String galleryPost(@RequestParam("image") MultipartFile image, Model m)
     {
         try {
             byte[] imgBytes= image.getBytes();
@@ -43,7 +47,13 @@ public class GalleryController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+//        catch(MaxUploadSizeExceededException e)
+//        {m.addAttribute("errorSize","File size exceed");
+//            return "gallery";
+//        }
 
+
+m.addAttribute("imageList",imageRepo.findAll());
         return "gallery";
     }
 }
---
+++ src/main/java/com/virinchi/controllerAdvice/ExceptionHandling.java
@@ -0,0 +1,19 @@
+package com.virinchi.controllerAdvice;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+@ControllerAdvice
+public class ExceptionHandling {
+
+    @ExceptionHandler(value= MaxUploadSizeExceededException.class)
+public String maxSizeHandler( MaxUploadSizeExceededException e,  Model m)
+{
+
+m.addAttribute("errorSize","File Size Exceeded");
+    return "gallery";
+}
+
+}
